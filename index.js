9066a9faa1a1a2cc1adc7df0254fd79a9141045f
calvintaw <190220508+calvintaw@users.noreply.github.com>
2025-07-18 21:30:56 +0630

    fix: Logic for DB querying and column names

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "http://localhost:3000", // or multiple origins in an array
+		origin: "http://localhost:3000", // [] for multiple origin
 		credentials: true,
 	},
 });
@@ -32,30 +32,86 @@ io.on("connection", (socket) => {
 	// join
 	socket.on("join", (room) => {
 		socket.join(room);
-		console.log("socket joined: ", room);
+		console.log("socket joined:", room);
 	});
-	// msg
-	socket.on("message", async ({ room, sender_id, sender_displayName, content, type = "text" }) => {
+
+	socket.on("create_dm", async ({}) => {});
+
+	// request friendship
+	socket.on("request_friendship", async ({ friend_id, user_id }) => {
 		try {
-			await sql`insert into messages (room_id, sender_id, sender_displayName, content, type) values (
-				${room},
-				${sender_id},
-				${sender_displayName},
-				${content},
-				${type}
-			)`;
+			await sql.begin(async (sql) => {
+				await sql`
+					INSERT INTO friends (user_id, friend_id, status)
+					VALUES (${user_id}, ${friend_id}, 'pending')
+				`;
+			});
+
+			console.log(`Success: request_friendship`);
+		} catch (error) {
+			console.error(error);
+		}
+	});
+
+	// accept friendship
+	socket.on("accept_friendship", async ({ friend_id, user_id, room_id }) => {
+		try {
+			await sql.begin(async (sql) => {
+				await sql`
+					UPDATE friends
+					SET status = 'accepted'
+					WHERE user_id = ${user_id} AND friend_id = ${friend_id}
+				`;
+
+				await sql`
+					INSERT INTO rooms (id, type)
+					VALUES (${room_id}, 'dm')
+					ON CONFLICT DO NOTHING
+				`;
+
+				await sql`
+					INSERT INTO room_members (room_id, user_id)
+					VALUES
+						(${room_id}, ${user_id}),
+						(${room_id}, ${friend_id})
+					ON CONFLICT DO NOTHING
+				`;
+			});
+
+			console.log(`Success: accept_friendship`);
+		} catch (error) {
+			console.error(error);
+		}
+	});
+
+	// send message
+	socket.on("message", async ({ room: room_id, sender_id, sender_display_name, content, type = "text" }) => {
+		try {
+			await sql.begin(async (sql) => {
+				const [{ id }] = await sql`
+					INSERT INTO messages (room_id, sender_id, sender_display_name, content, type)
+					VALUES (${room_id}, ${sender_id}, ${sender_display_name}, ${content}, ${type})
+					RETURNING id
+				`;
+
+				await sql`
+					UPDATE rooms
+					SET last_msg_id = ${id}
+					WHERE id = ${room_id}
+				`;
+			});
 
 			const msg = {
 				sender_id,
-				sender_displayName,
+				sender_display_name,
 				content,
-				createdAt: new Date().toISOString(),
+				created_at: new Date().toISOString(),
 			};
 
-			io.to(room).emit("message", msg);
-			console.log("Sent: ", msg);
+			io.to(room_id).emit("message", msg);
+			console.log("Sent:", msg);
 		} catch (error) {
-			console.error("insert msg failed ", error);
+			console.error("insert msg failed", error);
 		}
 	});
 
