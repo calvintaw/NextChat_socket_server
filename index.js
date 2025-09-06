02413b4564b58132f1142ba842241ad6a600d990
calvintaw <190220508+calvintaw@users.noreply.github.com>
2025-09-06 11:14:44 +0630

    update

--- index.js
+++ index.js
@@ -219,6 +219,7 @@ io.on("connection", (socket) => {
 	socket.on("refresh-contacts-page", (currentUser_id, targetUser_id) => {
 		io.to(currentUser_id).emit("refresh-contacts-page");
 		io.to(targetUser_id).emit("refresh-contacts-page");
+		
 	});
 
 	socket.on("leave", (room) => socket.leave(room));
