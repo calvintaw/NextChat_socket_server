2e8b36d5e5acd8017f577b760db907e350482ccf
calvin-taw <190220508+calvintaw@users.noreply.github.com>
2025-09-26 22:20:15 +0630

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ import "dotenv/config";
 
 import OpenAI from "openai";
 
-const openai = new OpenAI({
-	apiKey: process.env["OPENAI_API_KEY"],
+const openai = new OpenAI(
+	
+	{
+  baseURL: 'https://api.aimlapi.com/v1',
+		apiKey: process.env["OPENAI_API_KEY"],
 });
 
 if (!process.env.POSTGRES_URL) {
